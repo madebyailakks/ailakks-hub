bbd406b81eae212e02f450415326d2782630f01c
Ailakks <hola@cortmnzz.com>
2023-02-20 00:03:43 +0100

    added works list

--- src/components/list/works/works-list.jsx
+++ src/components/list/works/works-list.jsx
@@ -1,16 +1,53 @@
 import style from "./work-list.module.css";
 
-const projectsList = [
+const worksList = [
+    {
+        "name": "Widdlo",
+        "image": "widdlo.png",
+        "description": "",
+        "github": ""
+    },
+    {
+        "name": "Aguacate",
+        "image": "aguacate.png",
+        "description": "",
+        "github": ""
+    },
+    {
+        "name": "Hylaxe Studios",
+        "image": "hylaxe-studios.png",
+        "description": "",
+        "github": ""
+    },
+    {
+        "name": "Waterlands",
+        "image": "waterlands.png",
+        "description": "",
+        "github": ""
+    },
+    {
+        "name": "Wodmy",
+        "image": "wodmy.png",
+        "description": "",
+        "github": ""
+    },
+    {
+        "name": "Lookup",
+        "image": "lookup.png",
+        "description": "",
+        "github": ""
+    }
 ]
 
 export default function WorksList() {
     return (
         <div className={style.wrapper}>
-            {projectsList.map((value, key) =>
+            {worksList.map((value, key) =>
                 <div className={style.element} key={key}>
                     <div className={style.grid_wrap}>
                         <div className={style.element}>
-                            <p>{value.count}</p>
+                            <img className={style.avatar} src={require('../../../media/images/work/' + value.image)} />
+                            <p>{value.name}</p>
                         </div>
                     </div>
                 </div>
