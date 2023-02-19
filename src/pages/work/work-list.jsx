96f2cb338e41d47fedf1d3ecb32379116b946570
Ailakks <hola@cortmnzz.com>
2023-02-19 20:52:03 +0100

    creator list design fix

--- src/pages/work/work-list.jsx
+++ src/pages/work/work-list.jsx
@@ -82,7 +82,7 @@ export default function WorkList() {
                 <div className={style.element} key={key}>
                     <div className={style.grid_wrap}>
                         <div className={style.element}>
-                            <img className="avatar" src={require('../../media/creators/' + value.image)} />
+                            <img className={style.avatar} src={require('../../media/creators/' + value.image)} />
                             <h3 href={value.source}>{value.displayName}</h3>
                             <p>{value.count}</p>
                         </div>
