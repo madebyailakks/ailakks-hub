b6aac1324fe049d19e6b0a089311903f6df788c0
Ailakks <hola@cortmnzz.com>
2023-02-19 21:27:52 +0100

    added home component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,8 @@ import style from "./home.module.css";
 export default function Home() {
     return (
         <div className={style.header}>
-
+            <h1>Hola, soy Ailakks</h1>
+            <p>Tengo 16 años, soy de España, y continúo aprendo a diario diferentes lenguajes de programación y tecnologías. He trabajado para grandes creadores de contenido y estoy continuamente trabajando en proyectos nuevos.</p>
         </div>
     )
 }
--- src/pages/work/work.jsx
+++ src/pages/work/work.jsx
@@ -3,10 +3,15 @@ import WorkList from "../../components/list/work/work-list";
 
 export default function Work() {
     return (
-        <div className={style.header}>
+        <div>
             <h1>Creadores de contenido</h1>
-            <p>Estos son algunos de los creadores de contenido para los que he tenido la oportunidad de trabajar, ya sea directamente o en alguno de sus proyectos.</p>
-            <WorkList />
+            <div className={style.wrapper}>
+                <div className={style.text_wrap}>
+                    <p>Estos son algunos de los creadores de contenido para los que he tenido la oportunidad de trabajar, ya sea directamente o en alguno de sus proyectos.</p>
+                    <p>Muchos más creadores han disfrutado indirectamente de mis creaciones en proyectos mayores, como el HormiGeo, puedes ver más en proyectos :)</p>
+                </div>
+                <WorkList />
+            </div>
         </div>
     )
 }
