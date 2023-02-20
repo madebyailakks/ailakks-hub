6eff2290f606949e0efcf32229ff7119a37a35b4
Ailakks <hola@cortmnzz.com>
2023-02-20 17:09:23 +0100

    added transitions component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/header/header";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import Home from "./pages/home/home";
 import Work from "./pages/work/works";
 import ContentCreators from "./pages/content-creators/content-creators";
@@ -7,24 +7,31 @@ import Projects from "./pages/projects/projects";
 import NotFound from "./pages/not-found/not-found";
 import Footer from "./components/footer/footer";
 import Playlist from "./pages/playlist/playlist";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+
+import style from "./transition.module.css";
 
 function App() {
+    const location = useLocation();
+
     return (
         <div className="app">
             <Header/>
             <div className="body">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/content-creators" element={<ContentCreators/>}/>
-                        <Route path="/projects" element={<Projects/>}/>
-                        <Route path="/work" element={<Work/>}/>
-                        <Route path="/music" element={<Playlist />}/>
-                        <Route path="*" element={<NotFound/>}/>
-                    </Routes>
-                </BrowserRouter>
+                <TransitionGroup component={null}>
+                    <CSSTransition key={location.key} classNames={style.fade} timeout={300}>
+                        <Routes location={location}>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/content-creators" element={<ContentCreators/>}/>
+                            <Route path="/projects" element={<Projects/>}/>
+                            <Route path="/work" element={<Work/>}/>
+                            <Route path="/music" element={<Playlist/>}/>
+                            <Route path="*" element={<NotFound/>}/>
+                        </Routes>
+                    </CSSTransition>
+                </TransitionGroup>
             </div>
-            <Footer />
+            <Footer/>
         </div>
     );
 }
