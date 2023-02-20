72f76c61b96afa895fffb696205f224b734e800d
Ailakks <hola@cortmnzz.com>
2023-02-20 03:27:39 +0100

    added playlist data repeater

--- src/pages/playlist/playlist.jsx
+++ src/pages/playlist/playlist.jsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 import axios from 'axios';
 
 export default function Playlist() {
-    const [playlist, setPlaylist] = useState("");
+    const [playlist, setPlaylist] = useState(null);
 
     useEffect(() => {
         async function fetchPlaylist() {
@@ -15,10 +15,24 @@ export default function Playlist() {
         fetchPlaylist();
     }, []);
 
-    return (
-        <div className={style.wrapper}>
-            <h1>Esto está muy vacío</h1>
-            <p>{JSON.stringify(playlist)}</p>
-        </div>
-    )
+    if (playlist) {
+        return (
+            <div className={style.wrapper}>
+                {
+                    playlist.tracks.items.map((value, key) =>
+                        <div className={style.element} key={key}>
+                            {value.track.name}
+                            {
+                                value.track.artists.map((value, key) =>
+                                    <div className={style.element} key={key}>
+                                        {value.name}
+                                    </div>
+                                )
+                            }
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
 }
