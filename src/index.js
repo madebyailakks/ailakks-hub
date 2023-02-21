d63c188c96dfaa37ff2ed8e09ff45a7e1b2b4668
Ailakks <hola@cortmnzz.com>
2023-02-21 02:25:22 +0100

    added hash router basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {HashRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter basename="/">
+      <HashRouter basename={process.env.PUBLIC_URL}>
           <App />
       </HashRouter>
   </React.StrictMode>
