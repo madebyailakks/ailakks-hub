ee4d0603950e93e36275e6386aa7d0cf16fee910
Ailakks <hola@cortmnzz.com>
2023-02-22 17:58:30 +0100

    improved balloon overlay design

--- src/components/special/birthday/balloons-overlay/balloons-overlay.jsx
+++ src/components/special/birthday/balloons-overlay/balloons-overlay.jsx
@@ -15,7 +15,6 @@ export default function BalloonsOverlay() {
         return `
   background-color: rgba(${r},${g},${b},0.7);
   color: rgba(${r},${g},${b},0.7); 
-  box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
   margin: ${mt}px 0 0 ${ml}px;
   animation: float ${dur}s ease-in infinite
   `;
