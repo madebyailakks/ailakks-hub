e86ce2747266784bdf16ea292c7086df25dfc637
Ailakks <hola@cortmnzz.com>
2023-02-24 00:04:29 +0100

    fixed spotify redirect update

--- src/pages/playlist/playlist.jsx
+++ src/pages/playlist/playlist.jsx
@@ -25,6 +25,8 @@ export default function Playlist() {
             try {
                 const response = await axios.get(process.env.REACT_APP_API_ENDPOINT + "/token", { params: { code: searchParams.get('code') } });
                 setCookie('token', response.data.token);
+
+                fetchPlaylist();
             } catch (error) {
                 console.error(error);
             }
@@ -32,12 +34,27 @@ export default function Playlist() {
 
         if (getCookie('token')) {
             fetchPlaylist();
-        } else {
+        } else if (searchParams.get('code')) {
             fetchToken();
         }
     }, []);
 
-    if (getCookie('token')) {
+    if (!getCookie('token')) {
+        return (
+            <div>
+                <Header text="Música"/>
+                <div className="container">
+                    <div id={style.login} className={style.wrapper}>
+                        <div>
+                            <h2>Accede con tu cuenta de Spotify</h2>
+                            <p>Completa este sencillo paso con tu cuenta para ver la playlist.</p>
+                        </div>
+                        <a className="main_btn" href={`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&scope=playlist-read-private&redirect_uri=${encodeURIComponent(process.env.REACT_APP_SPOTIFY_REDIRECT_URI)}`}>Continuar a Spotify</a>
+                    </div>
+                </div>
+            </div>
+        )
+    } else {
         return (
             <div>
                 <Header text="Música"/>
@@ -61,21 +78,6 @@ export default function Playlist() {
                             }
                         </div> :
                         <p>{failed ? "No se han podido obtener los datos. Inténtalo de nuevo más tarde." : "Cargando los datos la de lista de reproducción..."}</p>}
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <Header text="Música"/>
-                <div className="container">
-                    <div id={style.login} className={style.wrapper}>
-                        <div>
-                            <h2>Accede con tu cuenta de Spotify</h2>
-                            <p>Completa este sencillo paso con tu cuenta para ver la playlist.</p>
-                        </div>
-                        <a className="main_btn" href={`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&scope=playlist-read-private&redirect_uri=${encodeURIComponent(process.env.REACT_APP_SPOTIFY_REDIRECT_URI)}`}>Continuar a Spotify</a>
-                    </div>
                 </div>
             </div>
         )
