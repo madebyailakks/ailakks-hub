4e92bac4672f0c939f4d7b8e6e5aa3cccfab15a1
Ailakks <hola@cortmnzz.com>
2023-02-26 00:02:25 +0100

    removed bday stuff

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,5 @@
 import style from "./home.module.css";
 import {Link} from "react-router-dom";
-import BalloonsOverlay from "../../components/special/birthday/balloons-overlay/balloons-overlay";
 import confetti from "https://cdn.skypack.dev/canvas-confetti@1";
 import {useCallback} from "react";
 
